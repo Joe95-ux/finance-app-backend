26ee94cd9f3e30b3bcde3d4bc4ed252fbfc6fae5
joe95-ux <ogorktabi@gmail.com>
2024-12-30 18:35:00 +0200

    setup for production

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import path from "path";
 import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js";
 import transactionRoutes from "./routes/transaction.js";
 import KPI from "./models/KPI.js";
 import Product from "./models/Product.js";
 import Transaction from "./models/Transaction.js";
-import { kpis, products, transactions} from "./data/data.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -22,16 +23,16 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({origin: "*", allowedHeaders: ['Content-Type']}));
+app.use(cors({ origin: "*", allowedHeaders: ["Content-Type"] }));
 
 /* ROUTES */
-app.use('/kpi', kpiRoutes);
+app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
 app.use("/transaction", transactionRoutes);
 
-
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
+const __dirname = path.resolve();
 mongoose
   .connect(process.env.MONGO_URL)
   .then(async () => {
@@ -44,3 +45,11 @@ mongoose
     //  Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
